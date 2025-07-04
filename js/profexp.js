2a8180e696b54ed8278d4164f9737b4a5c783c62
Sara Girard <sarag.bsa@gmail.com>
2025-07-04 04:46:34 -0400

    update to have a minimum of prior experience placeholders

--- js/profexp.js
+++ js/profexp.js
@@ -1,24 +1,127 @@
 // List of professional experience
-let profexp_list = [
-    {
-        years: "2022 - 2024",
-        role: "Senior Software Engineer",
-        company: "Fusion Technology LLC",
-        tools: [
-            "HTML/CSS",
-            "Bootstrap",
-            "JavaScript",
-            "React",
-            "Spring",
-            "Java",
-            "Azure"
-        ],
-        description: [
-            "Provide full stack development, maintenance, and system administration services for organizations",
-            "Triage customer issues, prioritize, and fix root causes, paying attention to severity and proper logging practices",
-            "Review and understand business system",
-            "Create requirements based on specific clients' needs",
-            "Build, schedule, and release new software and software improvements"
-        ]
-    }
+let profexp_list =
+    [
+        {
+            years: "2025 - Current",
+            role: "Owner, Software Engineer",
+            company: "Paditim Tech LLC",
+            tools: [
+
+            ],
+            description: [
+
+            ]
+        },
+        {
+            years: "2022 - 2024",
+            role: "Senior Software Engineer",
+            company: "Fusion Technology LLC",
+            tools: [
+                "Spring Boot",
+                "Java",
+                "JavaScript",
+                "Python",
+                "AWS"
+            ],
+            description: [
+                "Provide full stack development, maintenance, and system administration services for organizations",
+                "Triage customer issues, prioritize, and fix root causes, paying attention to severity and proper logging practices",
+                "Review and understand business system",
+                "Create requirements based on specific clients' needs",
+                "Build, schedule, and release new software and software improvements"
+            ]
+        },
+        {
+            years: "2021 - 2022",
+            role: "Software Engineer",
+            company: "Fusion Technology",
+            tools: [
+                "HTML/CSS",
+                "Bootstrap",
+                "JavaScript",
+                "Spring",
+                "Java",
+                "C#",
+                "Azure",
+                "MySQL"
+            ],
+            description : [
+
+            ]
+        },
+        {
+            years: "2020 - 2021",
+            role: "Lead Consultant, Government",
+            company: "Girard Consulting",
+            tools: [
+                "HTML/CSS",
+                "JavaScript",
+                "WordPress",
+                "WordPress",
+                "Python",
+                "Bootstrap"
+            ],
+            description: [
+                "Provide process and software control analysis, documentation, and provide recommendations for improvements to create Agile organizations",
+                "Use clients' requirements to develop targeted solutions to fit specific needs",
+                "Determine project deliverables and tasks based on milestone dates and deadlines",
+                "Work closely with all stakeholders to identify business issues",
+                "Identify issues requiring improvement and set processes in place to remedy concerns",
+                "Devise business analysis strategy, policies and proposals to meet organizational targets",
+                "Coordinate with clients to discuss new ways to boost business performance",
+                "Craft initiatives and solutions to deliver strategic objectives",
+                "Develop end-to-end project technical approach plans using client's data, systems, business and teams",
+                "Present business and systems options for business improvement to market consultancy services"
+            ]
+        },
+        {
+            years: "2018 - 2020",
+            role: "Director of Business Services",
+            company: "Lake Superior School District",
+            tools: [
+                "HTML/CSS",
+                "JavaScript",
+                "SQL",
+                "Databases",
+                "ERP",
+                "Office 365"
+            ],
+            description: [
+                "Review and refine organizational processes and technologies for redundancy, inefficiency, and waste",
+                "Implement strategies and technology to enhance stakeholder experience including introducing credit card program and electronic timesheet tracking and processing",
+                "Monitor relevant legal, regulatory, and ethical requirements and implement them into District policies and procedures",
+                "Create strategies to use technology to align with the local ways of working and industry best practices",
+                "Consult with District stakeholders to introduce new or improve existing technologies"
+            ]
+        },
+        {
+            years: "2016 - 2018",
+            role: "Account Specialist 2",
+            company: "South Washington County Schools",
+            tools: [
+                "Adobe",
+                "Javascript",
+                "WordPress",
+                "Databases",
+                "ERP",
+                "Office 365"
+            ],
+            description: [
+                "Spearhead an initiative to create live and accurate employee position controls for the District’s growing employment of over 3,000 employees",
+                "Administrate the implementation of Skyward ERP system district-wide",
+                "Advocate for improvements in District processes, technology and otherwise",
+                "Implement current software to increase productivity efficiency and accuracy"
+            ]
+        },
+        {
+            years: "2014 - 2018",
+            role: "Consultant",
+            company: "Serenity Consulting",
+            tools: [
+
+            ],
+            description: [
+
+            ]
+        }
 ];
